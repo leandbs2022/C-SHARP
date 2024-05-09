1e6f82d5bb74ac5e85d79f7588ded6878c6731a3
Leandro BSB <109764459+leandbs2022@users.noreply.github.com>
2024-05-09 12:13:50 -0300

    090520241213

--- MVC/LHPet/Controllers/HomeController.cs
+++ MVC/LHPet/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using LHPet.Models;
+using LHPet.models;
 
 namespace LHPet.Controllers;
 
@@ -15,6 +16,18 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+//instancias do tipo cliente
+        Cliente cliente1 = new Cliente(01,"Arthur A. Ferreira","857.032.950-41","arthur.antunes@sp.senai.br","Madruga");
+        Cliente cliente2 = new Cliente(02,"Willian","036.618.250-09","Willian@sp.senai.br","Bug");
+        Cliente cliente3 = new Cliente(03,"Ada","800.777.920.50","Ada@sp.senai.br","Byron");
+        Cliente cliente4 = new Cliente(04,"Linus","933.622.400.03","Linus@sp.senai.br","Pinguim");
+        Cliente cliente5 = new Cliente(05,"Grace ","911.702.988.00","Grace@sp.senai.br","Loboc");
+//instancias do tipo Fornecedor
+        Fornecedor fornecedor1 = new Fornecedor(01,"C# PET S/A","14.182.102/0001-00","arthur.antunes@sp.senai.br");
+        Fornecedor fornecedor2 = new Fornecedor(02,"CTRL ALT DOG","15.836.698/0001-57","Willian@sp.senai.br");
+        Fornecedor fornecedor3 = new Fornecedor(03,"BOOTPET INC","40.810.224/0001-83","Ada@sp.senai.br");
+        Fornecedor fornecedor4 = new Fornecedor(04,"TIK TOK DOGS","87.945.350/0001-09","Linus@sp.senai.br");
+        Fornecedor fornecedor5 = new Fornecedor(05,"BIFINHO FOREVER","10.760.614/0001-37","Grace@sp.senai.br");
         return View();
     }
 
