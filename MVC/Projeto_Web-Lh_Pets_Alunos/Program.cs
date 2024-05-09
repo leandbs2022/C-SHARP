1c0ea9950afb6c2de9bb67b24a7ca11e4f0939ef
Leandro BSB <109764459+leandbs2022@users.noreply.github.com>
2024-05-09 13:58:06 -0300

    090520241358

---
+++ MVC/Projeto_Web-Lh_Pets_Alunos/Program.cs
@@ -0,0 +1,18 @@
+namespace Projeto_Web_Lh_Pets_Alunos;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Projeto Web LH Pets versão 1");
+        app.UseStaticFiles();
+        app.MapGet("/index", (HttpContext contexto)=>{
+            contexto.Response.Redirect("index.html",false)
+        })
+
+        app.Run();
+    }
+}
